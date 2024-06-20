31d47547739f9a2a250679d5eeee3575c055b547
Felipe Martins <felipedesmartins@gmail.com>
2024-06-20 00:21:48 -0300

    Added getters and setters

--- core/Model/ModelFactory.php
+++ core/Model/ModelFactory.php
@@ -9,12 +9,60 @@ class ModelFactory
   private ModelSelect $modelSelect;
   private ModelUpdate $modelUpdate;
 
-  public function __construct(ModelTable $modelTable, ModelInsert $modelInsert, ModelSelect $modelSelect, ModelUpdate $modelUpdate)
+  public function __construct(?ModelTable $modelTable = null, ?ModelInsert $modelInsert = null, ?ModelSelect $modelSelect = null, ?ModelUpdate $modelUpdate = null)
+  {
+    $this->setModelTable($modelTable);
+    $this->setModelInsert($modelInsert);
+    $this->setModelSelect($modelSelect);
+    $this->setModelUpdate($modelUpdate);
+  }
+
+  public function getModelTable(): ModelTable
+  {
+    return $this->modelTable;
+  }
+
+  public function setModelTable(ModelTable $modelTable): self
   {
     $this->modelTable = $modelTable;
+
+    return $this;
+  }
+
+  public function getModelInsert(): ModelInsert
+  {
+    return $this->modelInsert;
+  }
+
+  public function setModelInsert(ModelInsert $modelInsert): self
+  {
     $this->modelInsert = $modelInsert;
+
+    return $this;
+  }
+
+  public function getModelSelect(): ModelSelect
+  {
+    return $this->modelSelect;
+  }
+
+  public function setModelSelect(ModelSelect $modelSelect): self
+  {
     $this->modelSelect = $modelSelect;
+
+    return $this;
+  }
+
+  public function getModelUpdate(): ModelUpdate
+  {
+    return $this->modelUpdate;
+  }
+
+  public function setModelUpdate(ModelUpdate $modelUpdate): self
+  {
     $this->modelUpdate = $modelUpdate;
+
+    return $this;
   }
 
   public function loadDataToInsert(array $data): self
