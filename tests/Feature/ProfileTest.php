69d7e24afb24daab9aa55dba90ad3655195a2a6b
Felipe Martins <felipedesmartins@gmail.com>
2024-12-09 07:34:16 -0300

    fix: tests considering name from Person

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -2,7 +2,8 @@
 
 namespace Tests\Feature;
 
-use App\Models\User;
+use App\Models\Users\Person;
+use App\Models\Users\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Volt\Volt;
 use Tests\TestCase;
@@ -13,7 +14,8 @@ class ProfileTest extends TestCase
 
     public function test_profile_page_is_displayed(): void
     {
-        $user = User::factory()->create();
+        $person = Person::factory()->create();
+        $user = User::find($person->user_id)->first();
 
         $response = $this->actingAs($user)->get('/profile');
 
@@ -26,7 +28,8 @@ class ProfileTest extends TestCase
 
     public function test_profile_information_can_be_updated(): void
     {
-        $user = User::factory()->create();
+        $person = Person::factory()->create();
+        $user = User::find($person->user_id)->first();
 
         $this->actingAs($user);
 
@@ -40,15 +43,17 @@ class ProfileTest extends TestCase
             ->assertNoRedirect();
 
         $user->refresh();
+        $person->refresh();
 
-        $this->assertSame('Test User', $user->name);
+        $this->assertSame('Test User', $person->name);
         $this->assertSame('test@example.com', $user->email);
         $this->assertNull($user->email_verified_at);
     }
 
     public function test_email_verification_status_is_unchanged_when_the_email_address_is_unchanged(): void
     {
-        $user = User::factory()->create();
+        $person = Person::factory()->create();
+        $user = User::find($person->user_id)->first();
 
         $this->actingAs($user);
 
@@ -66,7 +71,8 @@ class ProfileTest extends TestCase
 
     public function test_user_can_delete_their_account(): void
     {
-        $user = User::factory()->create();
+        $person = Person::factory()->create();
+        $user = User::find($person->user_id)->first();
 
         $this->actingAs($user);
 
@@ -84,7 +90,8 @@ class ProfileTest extends TestCase
 
     public function test_correct_password_must_be_provided_to_delete_account(): void
     {
-        $user = User::factory()->create();
+        $person = Person::factory()->create();
+        $user = User::find($person->user_id)->first();
 
         $this->actingAs($user);
 
