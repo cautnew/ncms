49157523d7649837c10b1bb91fcdd896c201d4e8
Felipe Martins <felipe.martins2@br.nestle.com>
2024-12-27 19:21:40 -0300

    feat: festing components

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -1,7 +1,18 @@
+<?php
+
+use PHTML\UL;
+use PHTML\TAG;
+
+$ul = new UL('font-sans antialiased');
+$ul->append([
+  TAG::li(html: 'Item 1'),
+  TAG::li(html: 'Item 2'),
+  TAG::li(html: 'Item 3')
+]);
+
+?>
+
 <x-first-layout>
-  <ul>
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-  </ul>
+  <x-slot name="pageTitle">Teste</x-slot>
+  <?= $ul ?>
 </x-first-layout>
--- resources/views/layouts/first.blade.php
+++ resources/views/layouts/first.blade.php
@@ -2,10 +2,14 @@
 
 use PHTML\HTML;
 use PHTML\BODY;
+use PHTML\HEAD;
+use PHTML\TAG;
 use PHTML\P;
 
 $layout = new HTML();
 $layout->setLang(str_replace('_', '-', app()->getLocale()));
+$layout->append($head = new HEAD());
+$head->append(TAG::tagTitle(($pageTitle ?? 'First Layout') . " - NCMS"));
 
 $layout->append($body = new BODY());
 
