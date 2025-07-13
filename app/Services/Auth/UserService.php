bd69ca31caaad60a27e192f1827a547d8e9e77c9
Felipe Martins <felipedesmartins@gmail.com>
2025-07-13 20:30:58 -0300

    First commit

---
+++ app/Services/Auth/UserService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services\Auth;
+
+use App\Http\Requests\Auth\LoginRequest;
+
+class UserService
+{
+  public function register(LoginRequest $request) {
+    //
+  }
+}
---
+++ routes/api_auth.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/login/token', function (Request $request) {
+    dd($request);
+});
