c5480ec5571a2ba59d63611379961db7aa01a110
Felipe Martins <felipedesmartins@gmail.com>
2026-03-07 12:36:31 -0300

    Last update

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+  public function register(RegisterRequest $request)
+  {
+    try {
+      $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+      ]);
+    } catch (Exception $e) {
+      return response()->json([
+        'message' => 'User could not be created.',
+        'error' => $e->getMessage(),
+        'error-code' => $e->getCode()
+      ], 500);
+    }
+
+    if ($user) {
+      return response()->json([
+        'message' => 'User successfully created.',
+        'id' => $user->id
+      ], 201);
+    }
+
+    return response()->json([
+      'message' => 'User could not be created.',
+      'error' => 'Unknown error.'
+    ], 502);
+  }
+
+  public function login(LoginRequest $request)
+  {
+    try {
+      $login = Auth::attempt($request->only(['email', 'password']), $request->remember);
+    } catch (Exception $e) {
+      return response()->json([
+        'message' => 'User could not login.',
+        'error' => $e->getMessage(),
+        'error-code' => $e->getCode()
+      ], 500);
+    }
+
+    if (!$login) {
+      return response()->json(
+        [
+          'message' => 'Invalid credentials.'
+        ],
+        401
+      );
+    }
+
+    $user = User::where('email', $request->email)->first();
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json(['token' => $token]);
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/login/token', function (Request $request) {
+    return response()->json([
+        'message' => 'User authenticated',
+        'user-id' => Auth::id()
+    ]);
+});
